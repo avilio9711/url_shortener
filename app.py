fa116df7b6c5bb836abbba51363356d4edf0fb3d
Avilio Brown <avilio.brown@gmail.com>
2021-04-13 23:37:30 +0530

    app.py, home.html updated, your_html created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('home.html', name='avilio')
 
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'Not valid'
 # if __name__=='__main__':
 #     app.run(debug=True)
