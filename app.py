77093a9e279f6f7fd74c4a2d03167773e9fb302a
Avilio Brown <avilio.brown@gmail.com>
2021-04-14 23:31:53 +0530

    files updated

--- app.py
+++ app.py
@@ -1,24 +1,67 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
+app.secret_key = 'sdasdsdfasdf'
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
         urls = {}
-        if os.path.exists
-        urls[request.form['code']] = {'url': request.form['url']}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+                session[request.form['code']] = True
+        if request.form['code'] in urls:
+            flash('That short name already been taken, Please select another name.')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form:
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/home/avilio/program/project/Python/url-shortener/static/user_files/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    urls = {}
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+    if code in urls:
+        if 'url' in urls[code]:
+            return redirect(urls[code]['url'])
+        else:
+            return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
+
 # if __name__=='__main__':
 #     app.run(debug=True)
