487380ccdf358ba5dc8551ebc0373a7a5f0c7df0
Avilio Brown <avilio.brown@gmail.com>
2021-04-14 15:59:37 +0530

    files updated, urls.json created

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os.path
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('home.html', name='avilio')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+        if os.path.exists
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return 'Not valid'
+        return redirect(url_for('home'))
 # if __name__=='__main__':
 #     app.run(debug=True)
